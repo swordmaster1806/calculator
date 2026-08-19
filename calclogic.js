8a0eab1f4b0f523215e287b8f6c82eed50b210d4
swordmaster1806 <265546703+swordmaster1806@users.noreply.github.com>
2026-08-19 19:53:10 +0530

    made a decent looking calculator with basic functions

--- calclogic.js
+++ calclogic.js
@@ -9,9 +9,9 @@ let op2Written = false;
 let operandWritten = false;
 let result = 0;
 
-const digits = document.querySelector(".digits");
 const display = document.querySelector(".display");
-const operatorSelected = document.querySelector(".operator");
+
+const calc = document.querySelector("div");
 
 
 function operate(opeerand1, operand, operrand2) {
@@ -30,54 +30,85 @@ function operate(opeerand1, operand, operrand2) {
 
 
 
-digits.addEventListener("click", (event) => {
+
+
+calc.addEventListener("click", (event) => {
+
+    if (event.target.classList[0] === "operator") {
+
+        if (event.target.textContent === "clear") {
+            operator = undefined;
+            operand1 = undefined;
+            operand2 = undefined;
+            op1Written = false;
+            op2Written = false;
+            operandWritten = false;
+            digitarray = [];
+
+        }
+
+        else if (op1Written && !op2Written && event.target.textContent !== "=") {
+
+            if (!operandWritten) {
+                operand1 = +digitarray.join('');
+                digitarray = [];
+            }
+
+            operandWritten = true;
+            operator = event.target.textContent;
+
+        }
+
+        else if (op2Written) {
+
+            operand2 = +digitarray.join('');
+            result = operate(operand1, operator, operand2);
+            console.log(result);
+
+            operand1 = result;
+
+
+            digitarray = `${operand1}`.split('');
+
+
+
+            op2Written = false;
+            operandWritten = false;
+
+            if (event.target.textContent !== "=") {
+                operator = event.target.textContent;
+                operandWritten = true;
+                digitarray = [];
+
+            }
+        }
+    }
+
+    else if(event.target.matches("button")){
     digitarray.push(event.target.textContent)
-
     op1Written = true;
-
     if (operandWritten) op2Written = true;
+    }
+
 });
 
 
-operatorSelected.addEventListener("click", (event) => {
 
 
-    if (op1Written && !op2Written && event.target.textContent !== "=") {
+document.body.addEventListener("click", () => {
+    let text = "";
 
-        if(!operandWritten){
-         operand1 = +digitarray.join('');
-         digitarray = [];       
-        }
+    if (op1Written && !operandWritten) text = `${digitarray.join('')}`;
 
-        operandWritten = true; 
-        operator = event.target.textContent;
+    else if (op1Written && !op2Written) text = `${operand1}` + " " + `${operator}`;
 
+    else if (op1Written && op2Written && event.target.classList != "operator") text = `${operand1}` + " " + `${operator}` + " " + `${digitarray.join('')}`;
+
+    else if (op1Written && op2Written && event.target.classList != "operator") {
+        if (event.target.textContent === "=") text = `${result}`;
+        else text = `${result} + " " + ${operator}`
     }
 
-    else if (op2Written) {
-
-        operand2 = +digitarray.join('');
-        result = operate(operand1, operator, operand2);
-        console.log(result);
-
-        operand1 = result;
-      
-
-        digitarray = `${operand1}`.split('');
-       
-        
-
-        op2Written = false;
-        operandWritten = false;
-
-        if (event.target.textContent !== "=") {
-            operator = event.target.textContent;
-            operandWritten = true;
-        }
-
-    }
+    display.textContent = text;
 
 })
-
-
-
